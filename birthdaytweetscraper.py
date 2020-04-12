0ff6ec709afb4865a1d666bedee4478b2c63976e
David <david.day@utexas.edu>
2020-04-12 14:57:47 -0500

    added 15 minute request cool-off, print statements

--- birthdaytweetscraper.py
+++ birthdaytweetscraper.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 text_query = 'happy {}th birthday'
 age = 18  # set age to search for
-count = 500  # set number of results to fetch
+count = 2000  # set number of results to fetch
 
 
 tweetCriteria = got.manager.TweetCriteria().setQuerySearch(text_query.format(age)).setMaxTweets(count)
 
 tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 
+print('{} tweets found.'.format(len(tweets)))
+
 tweet_user = [tweet.username for tweet in tweets]
 tweet_text = [tweet.text for tweet in tweets]
-
 
 tweet_data = pd.DataFrame({'username': tweet_user, 'tweet': tweet_text})
 
--- tweetcollector.py
+++ tweetcollector.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import re
 import GetOldTweets3 as got
+import time
+
+FIFTEEN_MIN_IN_SEC = 15 * 65  # add a little extra time to be safe
 
 age = 18  # age used as a label for each data sample that is created, *THIS SHOULD MATCH age IN birthdaytweetscraper.py*
+count = 10  # number of tweets to scrape from each user
+
 df = pd.read_csv('{}yo.csv'.format(age))
 users = []
 
@@ -16,21 +21,43 @@ for tweet in tweets:
     except IndexError:
         continue
 
+users = list(set(users))  # remove repeated users
+
 tweet_data = {}
 
-count = 10  # number of tweets to scrape from each user
+print('Getting {} tweets from each of {} users.'.format(count, len(users), age))
+
+userNum = 1
+
 for user in users:
+    # every 500 users, wait 15 minutes to avoid making too many HTTP requests and erroring out
+    if (userNum % 500) == 0:
+        print("*15 minute request cool-off*")
+        startTime = time.time()
+        currentTime = time.time()
+        while currentTime - startTime < FIFTEEN_MIN_IN_SEC:
+            currentTime = time.time()
+
     tweetCriteria = got.manager.TweetCriteria().setUsername(user).setMaxTweets(count)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
     tweet_text = [tweet.text for tweet in tweets]
 
-    print(user + "has " + str(len(tweet_text)) + " tweets, adding "
-          + str(count - len(tweet_text)) + " extra empty tweets.")
+    # print(user + "has " + str(len(tweet_text)) + " tweets, adding "
+    #      + str(count - len(tweet_text)) + " extra empty tweets.")
+    # while len(tweet_text) < count:  # add empty elements if necessary to make every sample same length
+    #    tweet_text.append('')
 
-    while len(tweet_text) < count:  # add empty elements if necessary to make every sample same length
-        tweet_text.append('')
+    if len(tweet_text) < count:  # don't use sample if user has less than 'count' tweets
+        continue
+    else:
+        print('{}: Getting tweets from {}...'.format(userNum, user))
+        userNum += 1
 
     tweet_data[user] = tweet_text
+
+print('Done!')
+
+print('{} samples created out of {} total users.'.format(len(tweet_data), len(users)))
 
 tweet_ds = pd.DataFrame(tweet_data).transpose().reset_index()  # original dataframe has users as a row, so transpose
 
@@ -38,4 +65,4 @@ tweet_ds.rename(columns={'index': 'user'}, inplace=True)  # after transposing, n
 
 tweet_ds.insert(0, 'age', [age] * tweet_ds.shape[0], True)  # add age label
 
-tweet_ds.to_csv('{}yodataset.csv'.format(age), index=False, encoding='utf-8')
+tweet_ds.to_csv('{}yo_dataset.csv'.format(age), index=False, encoding='utf-8')
